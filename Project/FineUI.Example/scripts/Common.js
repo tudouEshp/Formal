fc7a9517afd75508b649178b5f93e2a85b97c813
JayZeeWen <562230790@qq.com>
2015-03-03 17:08:00 +0800

    js

---
+++ Project/FineUI.Example/scripts/Common.js
@@ -0,0 +1,20 @@
+﻿function confirmL(msg, callBack) {
+    var div = document.createElement("div");
+    $(div).html(msg).dialog({
+        title: "提醒",
+        modal: true,
+        resizable: false,
+        buttons: {
+            "确定": function (e) {
+                $(this).dialog("close");
+                if (callBack != null) {
+                    callBack();
+                }
+
+            }, "取消": function (e) {
+                $(this).dialog("close");
+
+            }
+        }
+    });
+}
