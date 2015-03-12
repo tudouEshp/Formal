50e475f0ce0e5d20d8f5b6f92d7f6bc5f816c67c
JayZeeWen <562230790@qq.com>
2015-03-12 16:54:51 +0800

    EProduct
    not finish

--- Project/FineUI.Example/Electronic.ashx.cs
+++ Project/FineUI.Example/Electronic.ashx.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using EShop.Model;
+using EShop.BLL;
+using EShop;
 
 namespace 土豆购物
 {
@@ -13,8 +17,11 @@ namespace 土豆购物
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/html";
+            DataTable Eps = SqlHelper.ExecuteDataTable("select top 3 * from T_Products where CTID =7 order by ShelveDate");
+            DataTable logos = SqlHelper.ExecuteDataTable("select top 16 * from T_Products t left join T_Brand b  on t.BraID = b.BraID where CTID in (7,8,9)");
 
-            string html = CommandHelper.RenderHtml("Front/Electronic.html", null);
+            var data = new { Eps = Eps.Rows, Logos = logos.Rows };
+            string html = CommandHelper.RenderHtml("Front/Electronic.html", data);
             context.Response.Write(html);
         }
 
